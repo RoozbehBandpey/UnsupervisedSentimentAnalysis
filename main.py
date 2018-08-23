6661c58246dcf45907c5aa8ec2460bf9f1ef533f
Roozbeh Bandpey <roozbeh.bandpey@harman.com>
2018-08-23 19:30:16 +0200

    ongoing development for new data_helper

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
-import datetime
-import re
+# import datetime
+# import re
+#
+# from model import Model
+# from evaluation import Evaluation
+from data_helper import DataHelper
+# import nltk
+# import pickle
 
-from model import Model
-from evaluation import Evaluation
-import nltk
-import pickle
-
-from corpus import Corpus
-import winsound
-import os
-import sys
-import time
+# from corpus import Corpus
+# import winsound
+# import os
+# import sys
+# import time
 
 ################################################################################################
-m = Model()
-#pred = m.count_based_prediction()
-#pred = m.count_based_prediction_conjunction()
-pred = m.count_based_prediction_conjunction_pmi()
-#
-Eval = Evaluation()
-result = Eval.Evaluate( pred)
-print("\n")
-print(result[0])
-print("F-measure micro: "+result[1]+"   F-measure macro: "+result[2])
+# m = Model()
+# #pred = m.count_based_prediction()
+# #pred = m.count_based_prediction_conjunction()
+# pred = m.count_based_prediction_conjunction_pmi()
+# #
+# Eval = Evaluation()
+# result = Eval.Evaluate( pred)
+# print("\n")
+# print(result[0])
+# print("F-measure micro: "+result[1]+"   F-measure macro: "+result[2])
 ##################################################################################################
+d = DataHelper()
+x=d.load_stopwords("English_Stopwords.txt")
+print(len(x))
